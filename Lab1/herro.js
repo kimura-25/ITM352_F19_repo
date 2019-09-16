befd2e599f5b204fb053eb2c4ca795f61a251ec7
kimura-25 <kimura25@hawaii.edu>
2019-09-16 00:47:35 -1000

    BH3 done

--- Lab1/herro.js
+++ Lab1/herro.js
@@ -7,6 +7,6 @@ http.createServer(function (req, res) {
   res.write('<h1>Hello World!</h1>'); //write a response to the client
   res.write('<script>document.getElementById("id_h1").innerHTML="Goodbye World!";</script>');
   res.end(); //end the response
-}).listen(8081); //the server object listens on port 8080
+}).listen(8080); //the server object listens on port 8080
 
 console.log('Hello world HTTP server listening on localhost port 8080');
