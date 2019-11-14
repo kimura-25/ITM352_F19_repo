52178513e6a55582fb688eb116bed41261ca9d0b
kimura-25 <54603722+kimura-25@users.noreply.github.com>
2019-11-14 07:59:11 -0800

    11/14/19 6am hst

--- Kimura_KelseyAnn_Assignment1/server.js
+++ Kimura_KelseyAnn_Assignment1/server.js
@@ -1,13 +1,12 @@
 var express = require('express'); // run express
-const querystring = require('querystring');
-const product_data = require('./public/product_data');
+const querystring = require('querystring'); // requiring a query string - string of whatever is written in textbox
+const product_data = require('./public/product_data'); //using data from product_data.js
 
 var app = express(); //start express
 
-//getting the data from the form where action is '/purchase'
 
-app.get('/purchase', function (req, res, next) {
-  console.log(Date.now() + ': Purchase made from ip ' + req.ip + ' data: ' + JSON.stringify(req.query));
+app.get('/purchase', function (req, res, next) { //getting the data from the form where action is '/purchase' 
+  console.log(Date.now() + ': Purchase made from ip ' + req.ip + ' data: ' + JSON.stringify(req.query)); // logging the date, IP address, and query of the purchase (quantities written in textboxes) into console
 
   // Validating quantity data, go through each and check if good
   // If it ok, send to invoice. if not, send back to product page
@@ -31,9 +30,9 @@ app.get('/purchase', function (req, res, next) {
   if (hasValidQuantities == true && hasPurchases == true) { // if both hasValidQuantities and hasPurchases are true
     res.redirect('./invoice.html?' + querystring.stringify(req.query)); // redirect to the invoice page with the query entered in the form
   } else {    // if either hasValidQuantities or hasPurchases is false
-    req.query["hasValidQuantities"] = hasValidQuantities;
-    req.query["hasPurchases"] = hasPurchases;
-    console.log(req.query);
+    req.query["hasValidQuantities"] = hasValidQuantities; // request the query for hasValidQuantities
+    req.query["hasPurchases"] = hasPurchases; // request the query for hasPurchases
+    console.log(req.query); // log the query into the console
     res.redirect('./form.html?' + querystring.stringify(req.query)); // redirect to the form again, keeping the query that they wrote
   }
 
@@ -45,31 +44,32 @@ app.use(express.static('./public')); // create a static server using express fro
 var listener = app.listen(8080, () => { console.log('server started listening on port ' + listener.address().port) }); // having the server listen on port 8080
 
 //Creating the function checkQuantityTextbox()
-
-function checkQuantityTextbox() {
+function checkQuantityTextbox() { // the function checkQuantityTextbox
   errs_array = isNonNegInt(quantity_textbox.value, true); //errs_array is equal to the value of what is in the textbox if it is true
-  qty_textbox_message.innerHTML = errs_array.join(',');
+  qty_textbox_message.innerHTML = errs_array.join(','); // list out (join) the errors for the textbox quantity if it is not an integer
 }
 
-//Creating the isNonNegInt function, which checks to make sure the quantity is a positive integer (from Lab 12 and 13)
+//Creating the isNonNegInt function, which checks to make sure the quantity is a positive integer 
+//From Lab 12 and 13
 function isNonNegInt(q, returnErrors = false) { // creating function with variable q, when returnErrors is false
   errors = []; // assume no errors at first
   if (q == '') q = 0; // handle blank inputs as if they are 0
   if (Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
   if (q < 0) errors.push('Negative value!'); // Check if it is non-negative
   if (parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
-  return returnErrors ? errors : (errors.length == 0);
+  return returnErrors ? errors : (errors.length == 0); // return no errors if the errors length is 0
 }
 
-//Creating the process_form function, which checks to make sure that the quantity is an integer before processing the form (Lab 13 Ex.4)
+//Creating the process_form function, which checks to make sure that the quantity is an integer before processing the form 
+//From Lab 13 Ex.4
 function process_form(GET, response) { //creating function with variables GET and response
   if (typeof GET['purchase'] != 'undefined') { // if what is written in the textbox is defined
     for (i in products) {  // for whatever the product number is
       let q = GET[`quantity_textbox${i}`]; // q is equal to the quantity able to get from the textbox for that product
       if (isNonNegInt(q)) { //if quantity is an integer
         receipt += eval('`' + contents + '`'); // render template string
-      } else {
-        receipt += `<h3><font color="red">${q} is not a valid quantity for ${model}!</font></h3>`; //or else say it is not a valid quantity
+      } else { // if quantity is not an integer
+        receipt += `<h3><font color="red">${q} is not a valid quantity for ${model}!</font></h3>`; // say it is not a valid quantity
       }
     }
     response.send(receipt); //sending the receipt
